2e9e43e7d5e7af108fbeeca4fdd569d78eb1e58e
Vickie Soshnikova <vickie@soshnikov.com>
2023-06-27 00:37:30 +0300

    add burger and 768px adaptation

--- salatri/script/burger.js
+++ salatri/script/burger.js
@@ -1,5 +1,13 @@
-(document).ready(function() {
-    ('.menu__burger').click(function(event) {
-        ('.menu__burger','menu__content').toggleClass('active');
-    });
-});
+// (document).ready(function() {
+//     ('.menu__burger').click(function(event) {
+//         ('.menu__burger','menu__content').toggleClass('active');
+//     });
+// });
+
+let menuBtn = document.querySelector('.menuu-btn');
+let menu = document.querySelector('.menuu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
